01ebdefe8ef9f0038daef833049baca17f8fa55e
varuscritical900 <varuscrtical900@gmail.com>
2023-04-14 15:46:26 +0800

    trying to finish on time

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,6 +4,8 @@ import NavBar from './components/NavBar.vue';
 import Product from './components/Products.vue';
 import Features from './components/Features.vue';
 import Carousel from './components/Carousel.vue';
+import Partners from './components/Partners.vue';
+// import Owner from './components/Owner.vue';
 
 </script>
 
@@ -14,5 +16,7 @@ import Carousel from './components/Carousel.vue';
     <Product/>
     <Carousel/>
     <Features/>
+    <Partners/>
+    <!-- <Owner/> -->
   </div>
 </template>
---
+++ client/src/components/Partners.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="d-flex flex-column">
+        <h4 class="align-self-center text-body-tertiary">OUR HAPPY CLIENTS</h4>
+        <h1 class="align-self-center">Our Partners</h1>
+    </div>
+    <div class="partner-logos d-flex flex-row mb-3 mt-3">
+    <div class="partner-logo">
+      <img src="https://firebasestorage.googleapis.com/v0/b/educat-b9f44.appspot.com/o/partner1.png?alt=media&token=e0ed6056-ec15-46f6-a081-eab21133dfdd" alt="Partner Logo" class="img-fluid mx-3"/>
+    </div>
+    <div class="partner-logo">
+      <img src="https://firebasestorage.googleapis.com/v0/b/educat-b9f44.appspot.com/o/partner2.png?alt=media&token=155135b1-9cf9-425a-ba4a-975212e9182d" alt="Partner Logo" class="img-fluid"/>
+    </div>
+    <div class="partner-logo">
+      <img src="https://firebasestorage.googleapis.com/v0/b/educat-b9f44.appspot.com/o/partner3.png?alt=media&token=cf13e17f-1140-4590-b16d-6650d1f9729c" class="img-fluid mx-3"/>
+    </div>
+    <div class="partner-logo">
+      <img src="https://firebasestorage.googleapis.com/v0/b/educat-b9f44.appspot.com/o/partner4.png?alt=media&token=e7f8eee6-47fb-4948-880f-083d67b7301c" alt="Partner Logo" class="img-fluid mx-3"/>
+    </div>
+    <div class="partner-logo">
+      <img src="https://firebasestorage.googleapis.com/v0/b/educat-b9f44.appspot.com/o/partner5.png?alt=media&token=4a811328-ebf5-4194-9977-f8caa5b44610" alt="Partner Logo" class="img-fluid mx-3"/>
+    </div>
+    <div class="partner-logo">
+      <img src="https://firebasestorage.googleapis.com/v0/b/educat-b9f44.appspot.com/o/partner6.png?alt=media&token=70c415cb-e057-4a42-b75d-37fec0c83003" alt="Partner Logo" class="img-fluid mx-3"/>
+    </div>
+  </div>
+  </template>
+
+<style>
+.partner-logo {
+  filter: grayscale(100%);
+  transition: filter 0.3s ease-in-out;
+}
+.partner-logo:hover {
+  filter: none;
+}
+</style>
